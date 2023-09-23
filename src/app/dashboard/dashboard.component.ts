d08410f82f4394e656d6b92359ddd9c4f85c5e8b
XantX <hp_xantx@hotmail.com>
2023-09-23 15:47:35 -0500

    Color para la tabla

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,7 +9,7 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class DashboardComponent implements AfterViewInit {
   showFiller = false;
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'status', 'type', 'actions'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -19,31 +19,22 @@ export class DashboardComponent implements AfterViewInit {
 }
 
 export interface PeriodicElement {
-  name: string;
+  status: string;
   position: number;
-  weight: number;
-  symbol: string;
+  type: string;
+  actions: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-  { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-  { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-  { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-  { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-  { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-  { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-  { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-  { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-  { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-  { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+  { position: 1, status: 'Completed', type: 'video', actions: 'H' },
+  { position: 2, status: 'Process', type: 'image', actions: 'He' },
+  { position: 3, status: 'Completed', type: 'video', actions: 'Li' },
+  { position: 4, status: 'Completed', type: 'video', actions: 'Be' },
+  { position: 5, status: 'Completed', type: 'video', actions: 'B' },
+  { position: 6, status: 'Completed', type: 'image', actions: 'C' },
+  { position: 7, status: 'Completed', type: 'image', actions: 'He' },
+  { position: 8, status: 'Completed', type: 'video', actions: 'Li' },
+  { position: 9, status: 'Completed', type: 'video', actions: 'Be' },
+  { position: 10, status: 'Completed', type: 'video', actions: 'B' },
+  { position: 11, status: 'Completed', type: 'image', actions: 'C' },
 ];
